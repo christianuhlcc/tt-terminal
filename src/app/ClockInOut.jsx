a22e5ec5ba0d78a454be24935a6cb6b6bf2a3c9e
Daniel Bolivar <daniel.bolivar@personio.de>
2023-07-31 14:10:12 +0200

    Bearerrrrrr

--- src/app/ClockInOut.jsx
+++ src/app/ClockInOut.jsx
@@ -1,36 +1,14 @@
 'use client'
-import { useQuery } from '@tanstack/react-query';
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
 
-const getToken = () => {
-  const options = {
-    mode: 'no-cors',
-    method: 'POST',
-    headers: {accept: 'application/json', 'content-type': 'application/json', 'Access-Control-Allow-Origin': '*',},
-    body: JSON.stringify({
-      client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      client_id: process.env.NEXT_PUBLIC_CLIENT_ID
-    })
-  };
-  
-  return fetch('https://api.personio.de/v1/auth', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-} 
-
-function ClockInOutComponent() {
-
+function ClockInOutComponent({ bearerToken }) {
+  console.log("clock-in-props", bearerToken);
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [elapsedTime, setElapsedTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const [selectedName, setSelectedName] = useState('');
-  const [names] = useState(['John', 'Jane', 'Alice']);
-
-  const data = useQuery(['bearerToken'], getToken)
-  console.log('fetched bearer token', data)
+  const [selectedName, setSelectedName] = useState("");
+  const [names] = useState(["John", "Jane", "Alice"]);
 
   useEffect(() => {
     let interval;
@@ -63,55 +41,54 @@ function ClockInOutComponent() {
         name: selectedName,
       };
 
-
       sumbitAttendance(AttendanceEvent);
-    
-    // Clear elapsed time
-    setElapsedTime(0);
-  };
+
+      // Clear elapsed time
+      setElapsedTime(0);
+    }
   };
 
   const sumbitAttendance = async (event) => {
-      // Make API call to submit attendance event to Personio
-      try {
-        const response = await fetch('https://api.personio.de/v1/company/attendances', {
-          method: 'POST',
-          mode: 'no-cors',
+    // Make API call to submit attendance event to Personio
+    try {
+      const response = await fetch(
+        "https://api.personio.de/v1/company/attendances",
+        {
+          method: "POST",
+          mode: "no-cors",
           headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-            'authorization' : 'Bearer '
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+            authorization: "Bearer ",
             // Add any necessary headers for authentication or authorization
           },
           body: JSON.stringify(event),
-        });
-
-        if (response.ok) {
-          console.log('Attendance event submitted successfully');
-          console.log(response)
-          // Do something with the successful response, e.g., show a success message
-        } else {
-          console.error('Failed to submit attendance event');
-          console.log('Token', process.env.NEXT_PUBLIC_CLIENT_ID)
-          // Handle the error case, e.g., show an error message
         }
-      } catch (error) {
-        console.error('Error occurred while submitting attendance event:', error);
-        // Handle any network or request errors
+      );
+
+      if (response.ok) {
+        console.log("Attendance event submitted successfully");
+        // console.log(response);
+        // Do something with the successful response, e.g., show a success message
+      } else {
+        console.error("Failed to submit attendance event");
+        console.log("Token", process.env.NEXT_PUBLIC_CLIENT_ID);
+        // Handle the error case, e.g., show an error message
       }
+    } catch (error) {
+      console.error("Error occurred while submitting attendance event:", error);
+      // Handle any network or request errors
     }
-    
+  };
 
   const formatTime = (time) => {
-
-  
     const seconds = Math.floor((time / 1000) % 60);
     const minutes = Math.floor((time / 1000 / 60) % 60);
     const hours = Math.floor((time / 1000 / 60 / 60) % 24);
 
-    return `${selectedName} | ${hours.toString().padStart(2, '0')}:
-            ${minutes.toString().padStart(2, '0')}:
-            ${seconds.toString().padStart(2, '0')}`;
+    return `${selectedName} | ${hours.toString().padStart(2, "0")}:
+            ${minutes.toString().padStart(2, "0")}:
+            ${seconds.toString().padStart(2, "0")}`;
   };
 
   const handleNameChange = (event) => {
@@ -120,47 +97,45 @@ function ClockInOutComponent() {
 
   return (
     <div className="flex flex-col items-center">
-    <div className="my-4">Elapsed Time: {formatTime(elapsedTime)}</div>
-    <div className="relative inline-flex">
-      <select
-        value={selectedName}
-        onChange={handleNameChange}
-        className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500"
-      >
-        <option value="">Select Name</option>
-        {names.map((name) => (
-          <option key={name} value={name}>
-            {name}
-          </option>
-        ))}
-      </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg
-          className="fill-current h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
+      <div className="my-4">Elapsed Time: {formatTime(elapsedTime)}</div>
+      <div className="relative inline-flex">
+        <select
+          value={selectedName}
+          onChange={handleNameChange}
+          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:border-blue-500"
         >
-          <path
-            d="M10 12l-4-4h8l-4 4z"
-          />
-        </svg>
+          <option value="">Select Name</option>
+          {names.map((name) => (
+            <option key={name} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg
+            className="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 12l-4-4h8l-4 4z" />
+          </svg>
+        </div>
       </div>
+      <button
+        onClick={handleStart}
+        disabled={isRunning || !selectedName}
+        className="mt-4 bg-blue-500 hover:bg-blue-700 disabled:bg-grey-100 text-white font-bold py-2 px-4 rounded"
+      >
+        Start
+      </button>
+      <button
+        onClick={handleStop}
+        disabled={!isRunning}
+        className="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Stop
+      </button>
     </div>
-    <button
-      onClick={handleStart}
-      disabled={isRunning || !selectedName}
-      className="mt-4 bg-blue-500 hover:bg-blue-700 disabled:bg-grey-100 text-white font-bold py-2 px-4 rounded"
-    >
-      Start
-    </button>
-    <button
-      onClick={handleStop}
-      disabled={!isRunning}
-      className="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Stop
-    </button>
-  </div>
   );
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,32 @@
-'use client'
+import Image from "next/image";
+import ClockInOutComponent from "./ClockInOut";
 
-import {
-  QueryClient,
-  QueryClientProvider
-} from '@tanstack/react-query'
-import Image from 'next/image'
-import ClockInOutComponent from './ClockInOut'
+const getToken = async () => {
+  console.log("going in here", process.env.NEXT_PUBLIC_CLIENT_SECRET);
+  const options = {
+    mode: "no-cors",
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    }),
+  };
 
-// Create a client
-const queryClient = new QueryClient()
+  const result = await fetch("https://api.personio.de/v1/auth", options);
 
-function App() {
-  return (
-    // Provide the client to your App
-    <QueryClientProvider client={queryClient}>
-      <ClockInOutComponent />
-    </QueryClientProvider>
-  )
-}
+  return result.json();
+};
 
-export default function Home() {
-  return (
+export default async function Home() {
+  const data = await getToken();
+
+  return data ? (
     <main className="flex min-h-screen flex-col items-center justify-between p-48">
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -32,13 +36,11 @@ export default function Home() {
           height={37}
           priority
         />
-        
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[30px] before:w-[480px] ">
-      
-        <App />
+        <ClockInOutComponent bearerToken={data.data.token} />
       </div>
     </main>
-  )
+  ) : null;
 }
